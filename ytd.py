87f061028e87e2e94710ef76bae7c344fba63ad3
Arthur McLain <mclain.it@gmail.com>
2023-05-31 20:04:13 +0200

    feat(ytd): add "verbose" option

--- ytd.py
+++ ytd.py
@@ -11,16 +11,14 @@ def ytdownload_direct():
 
     # Get the video from the Link
     if len(sys.argv) == 1:
-        link = input("Enter the Link: ")
+        link = input('Enter the Link: ')
     else:
         link = sys.argv[1]
 
-    print('Downloading...')
-    ytdownload(link)
-    print('Done')
+    ytdownload(link, outpath="songs", verbose=True)
 
 
-def ytdownload(link, outpath='out'):
+def ytdownload(link, outpath='out', verbose=False):
     """
     Download a video from YouTube as mp3
 
@@ -34,19 +32,28 @@ def ytdownload(link, outpath='out'):
     Raises:
         None
     """
+    if verbose:
+        print(f'Visiting {link}...')
+
     # Create an object
     yt = YouTube(link)
 
     # Get the right stream
     ys = yt.streams.filter(only_audio=True)[0]  # [-1] for best quality
-
-    # Download
-    ys.download(outpath)
-
-    # Convert mp4 to mp3
     default_filename = ys.default_filename
     title = yt.title
+
+    # Download
+    if verbose:
+        print(f'Downloading "{title}" as mp4... ', end='')
+    ys.download(outpath)
+    if verbose:
+        print('Done')
+
+    # Convert mp4 to mp3
     new_filename = title + '.mp3'
+    if verbose:
+        print(f'Converting "{title}" to mp3... ', end='')
     subprocess.run(
         [
             'ffmpeg',
@@ -59,8 +66,15 @@ def ytdownload(link, outpath='out'):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.STDOUT,
     )
+    if verbose:
+        print('Done')
+
     # Delete mp4
+    if verbose:
+        print(f'Deleting "{title}.mp4"... ' )
     subprocess.run(['rm', os.path.join(outpath, default_filename)])
+    if verbose:
+        print(f'Success! The file "{title}.mp3" can be found in the "{outpath}" folder.')
 
 
 if __name__ == '__main__':
