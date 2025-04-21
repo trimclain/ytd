747cd2496e9732cc98e206d152edf88d5a0baff9
Arthur McLain <mclain.it@gmail.com>
2025-04-21 22:24:11 +0200

    feat(ytd): add loading animations

--- ytd.py
+++ ytd.py
@@ -7,6 +7,10 @@
 
 import os
 import sys
+from itertools import cycle
+from shutil import get_terminal_size
+from threading import Thread
+from time import sleep
 
 import yt_dlp
 
@@ -16,11 +20,14 @@ def ytd():
 
     if len(sys.argv) == 1:
         url = input("Enter the URL: ")
+        print("\033[F", end='', flush=True)  # Move cursor up one line
+        print("\033[K", end='', flush=True)  # Clear the entire line
     else:
         url = sys.argv[1]
+    print("Entered URL: " + url, flush=True)
 
     running_from = sys.path[0]
-    if running_from == os.path.expanduser('~/.local/bin'):
+    if running_from == os.path.expanduser("~/.local/bin"):
         download_path = os.path.expanduser("~/songs")
     else:
         download_path = os.path.abspath("./songs")
@@ -66,15 +73,21 @@ def yt_download_audio(url, download_path="out", verbose=False):
         return
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        if verbose:
+            info_loader = Loader("Fetching track details from YouTube...", " ").start()
         info_dict = ydl.extract_info(url, download=False)
+        if verbose:
+            info_loader.stop()
+
         video_title = info_dict.get("title", None)
         if verbose:
-            print(f"Downloading '{video_title}.mp3' to the directory '{
-                  download_path}'...", end=" ", flush="True")
+            download_loader = Loader(
+                f"Downloading '{video_title}.mp3' to the directory '{download_path}'..."
+            ).start()
         # I know this is slower but whatever it's pretty
         ydl.download([url])
         if verbose:
-            print("Done")
+            download_loader.stop()
 
 
 class MyLogger:
@@ -94,6 +107,64 @@ class MyLogger:
 
     def error(self, msg):
         print(msg)
+
+
+# Credit: https://stackoverflow.com/a/66558182
+class Loader:
+    def __init__(self, desc="Loading...", end="Done!", timeout=0.1):
+        """
+        A loader-like context manager
+
+        Args:
+            desc (str, optional): The loader's description. Defaults to "Loading...".
+            end (str, optional): Final print. Defaults to "Done!".
+            timeout (float, optional): Sleep time between prints. Defaults to 0.1.
+
+        Usage:
+            with Loader("Loading with context manager..."):
+                for i in range(10):
+                    sleep(0.25)
+
+            loader = Loader("Loading with object...", "That was fast!", 0.05).start()
+            for i in range(10):
+                sleep(0.25)
+            loader.stop()
+        """
+        self.desc = desc
+        self.end = end
+        self.timeout = timeout
+
+        self._thread = Thread(target=self._animate, daemon=True)
+        # More animations: https://github.com/Silejonu/bash_loading_animations/blob/main/bash_loading_animations.sh
+        # self.steps = ["|", "/", "-", "\\"]
+        self.steps = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
+        # self.steps = ["[■□□□□□□□□□]", "[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+        self.done = False
+
+    def start(self):
+        self._thread.start()
+        return self
+
+    def _animate(self):
+        for c in cycle(self.steps):
+            if self.done:
+                break
+            print(f"\r{self.desc} {c}", flush=True, end="")
+            sleep(self.timeout)
+
+    def stop(self):
+        self.done = True
+        cols = get_terminal_size((80, 20)).columns
+        print("\r" + " " * cols, end="", flush=True)
+        # print(f"\r{self.end}", flush=True)
+        print(f"\r{self.desc} {self.end}", flush=True)
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, exc_type, exc_value, tb):
+        # handle exceptions with those variables ^
+        self.stop()
 
 
 if __name__ == "__main__":
